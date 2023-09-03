b6e3123e41dfee6d4e8cc204198b86c91b09bee6
jusruizsa <sruizs.jr@gmail.com>
2023-09-03 01:37:14 -0500

    add generated fake user and payment data

---
+++ api/data/generate.py
@@ -0,0 +1,40 @@
+from faker import Faker
+from core.models import User
+
+
+fake = Faker()
+u = User.objects.get(id=1)
+
+#GENERAR DATOS FAKE CC
+
+payment_data = []
+for i in range(10):
+    payment_record = {
+        'number': fake.credit_card_number(),
+        'valid_thru': fake.credit_card_expire(date_format='%Y/%m/%d'),
+        'cvv': fake.credit_card_security_code()
+    }
+
+    payment_data.append(payment_record)
+
+with open('payment.json', 'w') as fp:
+    fp.writelines(payment_data)
+
+
+#GENERAR DATOS DE USUARIO
+user = User
+
+user_data = []
+
+for i in range(5):
+    user_record = {
+        'name': fake.first_name(),
+        'lastname': fake.last_name(),
+        'birthday': fake.date_of_birth(minimum_age=18),
+        'email': fake.email()
+    }
+
+    user_data.append(user_record)
+
+with open('user.json', 'w') as fp:
+    fp.writelines(user_data.__str__())
