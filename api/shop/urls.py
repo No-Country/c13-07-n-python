e98d6471fda103e6d3eb2e2d62d341f70025151b
jusruizsa <sruizs.jr@gmail.com>
2023-09-03 01:38:11 -0500

    deleted unused files

--- api/shop/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from shop.views import CartItemAPIView, ProductAPIView, PurchaseAPIView
-
-urlpatterns = [
-    path('purchase/', PurchaseAPIView.as_view(), name='purchase_view'),
-    path('cart_item/', CartItemAPIView.as_view(), name='cart_item_view'),
-    path('product/', ProductAPIView.as_view(), name='product_view'),
-]
