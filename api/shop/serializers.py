a9cf2af0ce3ced07248260a84d2c8c22a9a7f19d
jusruizsa <sruizs.jr@gmail.com>
2023-09-03 22:41:21 -0500

    add Cart and CartItem methods

--- api/shop/serializers.py
+++ api/shop/serializers.py
@@ -1,17 +1,32 @@
 from rest_framework import serializers
-from .models import CartItem, Product, Purchase
+from .models import Cart, CartItem, Product, Purchase
+
 
 class CartItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = CartItem
         fields = '__all__'
 
+    def create(self, validated_data):
+        return super().create(validated_data)
+
+
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
 
+
 class PurchaseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Purchase
         fields = '__all__'
+
+
+class CartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cart
+        fields = '__all__'
+
+    def create(self, validated_data):
+        return super().create(validated_data)
