aa2309ec81a6e1aef6988b6b1307c3d552cec0d0
Nahuel Lupe <nahuellupe@hotmail.com>
2023-09-12 17:22:25 -0300

    actualizacion api

--- api/shop/serializers.py
+++ api/shop/serializers.py
@@ -1,15 +1,32 @@
 from rest_framework import serializers
-from .models import Product
+from .models import Cart, CartItem, Product, Purchase
 
-=======
-from .models import CartItem, Product
 
 class CartItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = CartItem
         fields = '__all__'
 
+    def create(self, validated_data):
+        return super().create(validated_data)
+
+
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
+
+
+class PurchaseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Purchase
+        fields = '__all__'
+
+
+class CartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cart
+        fields = '__all__'
+
+    def create(self, validated_data):
+        return super().create(validated_data)
